da2a66b356b54bd2d93b6a5a770372ab806762e0
Govorunb <Govorunb@users.noreply.github.com>
2025-12-01 07:58:37 +0000

    +0.1 nicer

--- rust/src/y25/days/day1.rs
+++ rust/src/y25/days/day1.rs
@@ -27,19 +27,19 @@ L82
         ]
     ],
     solve = |input, part| {
-        let r = Regex::new(r#"(?<d>L|R)(?<m>\d+)"#).unwrap();
+        let r = Regex::new(r#"(?<d>[LR])(?<m>\d+)"#).unwrap();
         let instructions = r.captures_iter(input)
             .map(|c| {
                 let mut amt = c.isize("m");
-                if c.str("d") == "L"  {
+                if c.str("d") == "L" {
                     amt = -amt;
                 }
                 amt
             });
         match part {
             Part::One => {
-                instructions.fold((50, 0), |(pos, clicks), el| {
-                    let mut new_pos = pos + el;
+                instructions.fold((50, 0), |(pos, clicks), amt| {
+                    let mut new_pos = pos + amt;
                     if new_pos < 0 {
                         new_pos += 100;
                     }
@@ -53,9 +53,12 @@ L82
                 }).1
             },
             Part::Two => {
-                instructions.fold((50, 0), |(pos, clicks), el| {
-                    let mut new_pos = pos + el;
+                instructions.fold((50, 0), |(pos, clicks), amt| {
+                    let mut new_pos = pos;
                     let mut new_clicks = clicks;
+
+                    new_clicks += amt.abs() as usize / 100; // there are some outliers >100 (like L900)
+                    new_pos += amt % 100;
                     while new_pos < 0 {
                         new_clicks += 1;
                         new_pos += 100;
@@ -65,15 +68,15 @@ L82
                         new_pos -= 100;
                     }
                     // 5 -> 0 (click!)
-                    if new_pos == 0 && el < 0 {
+                    if new_pos == 0 && amt < 0 {
                         new_clicks += 1;
                     }
                     // 0 -> -5 (95) (no click!)
-                    if pos == 0 && el < 0 {
+                    if pos == 0 && amt < 0 {
                         new_clicks -= 1;
                     }
                     if !cfg!(test) {
-                        println!("from {pos} {el} to {} ({new_pos}) {} clicks", pos+el, new_clicks - clicks);
+                        println!("from {pos} {amt} to {} ({new_pos}) {} clicks", pos+amt, new_clicks - clicks);
                     }
                     (new_pos, new_clicks)
                 }).1
