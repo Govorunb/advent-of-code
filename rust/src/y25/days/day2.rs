b4c447d5b71a564db2fa9b32eb109eb722c0a8d5
Govorunb <Govorunb@users.noreply.github.com>
2025-12-02 09:13:54 +0000

    🤏

--- rust/src/y25/days/day2.rs
+++ rust/src/y25/days/day2.rs
@@ -8,7 +8,12 @@ aoc_day!(
     examples = [
 "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,\
 1698522-1698528,446443-446449,38593856-38593862,565653-565659,\
-824824821-824824827,2121212118-2121212124"
+824824821-824824827,2121212118-2121212124",
+// additional challenges
+"11-42,95-115,998-7012,1188511880-2188511890,222220-222224,1698522-1698528,446443-646449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2321212124",
+"11-42,95-115,998-7012,222220-222224,446443-646449,1698522-1698528,38593856-38593862,824824821-824824827,1188511880-2321212124,202001202277-532532532530",
+// death
+"98765432-1234567890,1000000000000000000000000-1500000000000000000000000,988970940900875998011400-1050032916531789321707634,123456789012345678901234567890-234567890123456789012345678901",
     ],
     tests = [
         test_cases![
@@ -25,6 +30,8 @@ aoc_day!(
             ("2121212118-2121212124", 0),
             (Self::EXAMPLES[0], 1227775554),
             (Self::INPUT, 22062284697),
+            (Self::EXAMPLES[1], 21327161532716),
+            (Self::EXAMPLES[2], 121412594604227157),
         ],
         test_cases![
             ("11-22", [11,22].iter().sum()),
@@ -40,19 +47,23 @@ aoc_day!(
             ("2121212118-2121212124", 2121212121),
             (Self::EXAMPLES[0], 4174379265),
             (Self::INPUT, 46666175279),
+            // too long
+            // (Self::EXAMPLES[1], 21346784611163),
+            // (Self::EXAMPLES[2], 0),
         ]
     ],
     solve = |input, part| {
-        let pow10_table = (0..=24).map(|i| 10usize.pow(i)).collect_vec();
-        let factors_table = (0..24).map(|i| primes::factors(i)).collect_vec();
+        let pow10_table = (0..=15).map(|i| 10usize.pow(i)).collect_vec();
+        let factors_table = (0..15).map(|i| primes::factors(i)).collect_vec();
         let r = Regex::new(r#"(?<a>\d+)-(?<b>\d+)"#).unwrap();
         let ids = input.par_split(',')
             .map(|pair| {
+                // parsing later might save a negligible amount of time on log10
                 let c = r.captures(pair).unwrap();
                 (c.usize("a"), c.usize("b"))
             });
         // tried fancy-regex for backtracking
-        // /^(\d+)\1$/ and /^(\d+)\1+$/ are not faster (thank god)
+        // /^(\d+)\1$/ and /^(\d+)\1+$/ are way slower (thank god)
         match part {
             Part::One => {
                 ids.map(|(start, end)| {
@@ -60,8 +71,7 @@ aoc_day!(
                     let digits_start = digits(start);
                     let digits_end = digits(end);
                     let mut total = 0;
-                    
-                    // TODO: 20-2000? (luckily wasn't in input?)
+
                     let even_digits = if digits_start % 2 == 0 {
                         digits_start
                     } else if digits_end % 2 == 0 {
@@ -91,6 +101,11 @@ aoc_day!(
                             // count += 1;
                         }
                     }
+                    // FIXME: very very lucky that the input was kind
+                    // e.g. 30-40000
+                    // picks even_digits from start (2)
+                    // half_cut is 10 -> start_top..=end_top is 3..=4000
+                    // then, top * half_cut + top = 1234 * 10 + 1234 (which is obviously absurd and not a double)
                     
                     // let digits_maybe = (digits_start == digits_end).then_some(digits_start);
                     // let results = (start..=end).into_iter()
@@ -102,7 +117,7 @@ aoc_day!(
                     //         0
                     //     }).filter(|&e| e > 0).collect_vec();
                     // let sum = results.iter().sum();
-                    // println!("{start}..{end}:\n\tres: {sum}|{total}\n\tcount: {}|{count}", results.len());
+                    // println!("verify {start}..{end}:\n\tsum: {sum}|{total}\n\tcount: {}|{count}", results.len());
                     // assert!(results.len() == count);
                     // assert!(sum == total);
                     total
@@ -132,7 +147,7 @@ aoc_day!(
 );
 
 fn digits(num: usize) -> usize {
-    (num as f64).log10().ceil() as usize
+    num.checked_ilog10().unwrap_or(0) as usize + 1
 }
 
 #[inline]
