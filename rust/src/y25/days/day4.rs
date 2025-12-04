f24abdb03543e0d68e1765a5fd3c494e7cbf1f17
Govorunb <Govorunb@users.noreply.github.com>
2025-12-04 05:16:07 +0000

    y25d4

--- rust/src/y25/days/day4.rs
+++ rust/src/y25/days/day4.rs
@@ -3,25 +3,59 @@ use crate::*;
 aoc_day!(
     day = 4,
     output = usize,
-    examples = [""],
+    examples = [
+"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.",
+    ],
     tests = [
         test_cases![
-            (Self::EXAMPLES[0], 0),
-            // (Self::INPUT, 0),
+            (Self::EXAMPLES[0], 13),
+            (Self::INPUT, 1344),
         ],
         test_cases![
-            // (Self::EXAMPLES[0], 0),
+            (Self::EXAMPLES[0], 43),
             // (Self::INPUT, 0),
         ]
     ],
     solve = |input, part| {
-        let lines = input.lines();
+        let mut grid = input.parse::<Grid<char>>().unwrap();
         match part {
             Part::One => {
-                lines.count()
+                grid.cells()
+                    .filter(|&(_, c)| *c == '@')
+                    .filter(|(pos, _)| pos.around()
+                        .filter(|p| matches!(grid.get(&p), Some('@')))
+                        .count() < 4)
+                    .count()
             },
             Part::Two => {
-                0
+                let mut removed = 0;
+                loop {
+                    let removable = grid.cells()
+                        .filter(|&(_, c)| *c == '@')
+                        .filter(|(pos, _)| pos.around()
+                            .filter(|p| matches!(grid.get(&p), Some('@')))
+                            .count() < 4)
+                        .collect_vec();
+                    if removable.len() == 0 {
+                        break;
+                    }
+                    removed += removable.len();
+                    let mut grid2 = grid.clone();
+                    for r in removable {
+                        grid2[r.0] = '.';
+                    }
+                    grid = grid2;
+                }
+                removed
             }
         }
     }
