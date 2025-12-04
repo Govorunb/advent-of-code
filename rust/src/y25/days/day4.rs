896f4bacf5388af638dab8408ac25043f24fbc75
Govorunb <Govorunb@users.noreply.github.com>
2025-12-04 06:22:06 +0000

    less dumb

--- rust/src/y25/days/day4.rs
+++ rust/src/y25/days/day4.rs
@@ -22,41 +22,58 @@ aoc_day!(
         ],
         test_cases![
             (Self::EXAMPLES[0], 43),
-            // (Self::INPUT, 0),
+            (Self::INPUT, 8112),
         ]
     ],
     solve = |input, part| {
-        let mut grid = input.parse::<Grid<char>>().unwrap();
+        let mut grid: Grid<_> = input.parse().unwrap();
         match part {
             Part::One => {
                 grid.cells()
-                    .filter(|&(_, c)| *c == '@')
-                    .filter(|(pos, _)| pos.around()
-                        .filter(|p| matches!(grid.get(&p), Some('@')))
-                        .count() < 4)
+                    .filter(|&(pos, c)| c == &'@' && accessible(&pos, &grid))
                     .count()
             },
             Part::Two => {
                 let mut removed = 0;
+                let mut removable = vec![];
+                let mut next_check = FxHashSet::default();
                 loop {
-                    let removable = grid.cells()
-                        .filter(|&(_, c)| *c == '@')
-                        .filter(|(pos, _)| pos.around()
-                            .filter(|p| matches!(grid.get(&p), Some('@')))
-                            .count() < 4)
-                        .collect_vec();
+                    debug_assert!(removable.is_empty());
+                    // removing a roll only affects rolls that were around
+                    // therefore, we don't have to check the full grid
+                    if next_check.len() == 0 {
+                        removable.extend(grid.bounds().into_iter()
+                            .filter(|pos| grid[pos] == '@' && accessible(&pos, &grid))
+                        );
+                    } else {
+                        removable.extend(next_check.drain()
+                            .filter(|pos| accessible(&pos, &grid))
+                        );
+                    }
                     if removable.len() == 0 {
                         break;
                     }
                     removed += removable.len();
-                    let mut grid2 = grid.clone();
-                    for r in removable {
-                        grid2[r.0] = '.';
+                    for p in &removable {
+                        grid[p] = '.';
                     }
-                    grid = grid2;
+                    
+                    debug_assert!(next_check.is_empty());
+                    next_check.extend(removable.drain(..)
+                        .flat_map(|p| p.around())
+                        .filter(|p| matches!(grid.get(&p), Some('@')))
+                    );
                 }
                 removed
             }
         }
     }
 );
+
+fn accessible(pos: &Vector2, grid: &Grid<char>) -> bool {
+    pos.around()
+        .filter(|p| matches!(grid.get(p), Some('@')))
+        // you might think: count() will iterate up to all 8, we only care about 4
+        // but that case seems to already be optimized (at the very least, .nth(3).is_none() and .skip(3).next().is_none() are much slower)
+        .count() < 4
+}
