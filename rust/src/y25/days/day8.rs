0727a069e603e4a6354b90f7ca16ec27b40e6126
Govorunb <Govorunb@users.noreply.github.com>
2025-12-08 07:10:45 +0000

    sad

--- rust/src/y25/days/day8.rs
+++ rust/src/y25/days/day8.rs
@@ -47,15 +47,15 @@ aoc_day!(
         let pairs = pts.clone().into_iter()
             .pairwise()
             .sorted_by_cached_key(|(a,b)| euclid_dist_sqr_3d(*a,*b));
+        let mut circuits = pts.into_iter()
+            .map(|p| vec![p])
+            .collect_vec();
         match part {
             Part::One => {
+                // completely diabolical to have different KINDS of cutoffs for the example and the actual input
                 let cables = if input.lines().count() < 100 {9} else {999};
-                let mut circuits = pts.into_iter()
-                    .map(|p| vec![p])
-                    .collect_vec();
                 let mut connected = 0;
                 for (a, b) in pairs.take(1000) {
-                    // println!("pair {i}: {a},{b}");
                     if connect_pair(&mut circuits, (a,b)) {
                         connected += 1;
                         if connected >= cables {
@@ -63,20 +63,13 @@ aoc_day!(
                         }
                     }
                 };
-                println!("{}", circuits.len());
                 circuits.iter()
                     .map(|c| c.len())
-                    // .sorted_by_key(|c| c.len())
-                    .sorted()
-                    .rev()
+                    .sorted().rev()
                     .take(3)
-                    // .inspect(|c| println!("{1} {0}", PrintVec(c.iter().collect_vec()), c.len()))
                     .product::<usize>()
             },
             Part::Two => {
-                let mut circuits = pts.into_iter()
-                    .map(|p| vec![p])
-                    .collect_vec();
                 for (a, b) in pairs {
                     connect_pair(&mut circuits, (a,b));
                     if circuits.len() == 1 {
@@ -88,12 +81,6 @@ aoc_day!(
         }
     }
 );
-
-fn manhattan_dist_3d(a: Vector3, b: Vector3) -> usize {
-    (a.x - b.x).abs() as usize
-    + (a.y - b.y).abs() as usize
-    + (a.z - b.z).abs() as usize
-}
 
 fn euclid_dist_sqr_3d(a: Vector3, b: Vector3) -> u128 {
     (b.x - a.x).pow(2) as u128
@@ -117,37 +104,19 @@ fn connect_pair(circuits: &mut Vec<Vec<Vector3>>, pair: (Vector3, Vector3)) -> b
         .find_position(|c| c.contains(b))
         .map(|(i, _)| i);
     match (c_has_0, c_has_1) {
-        (None, None) => {
-            // println!("new circuit ({},{})", a, b);
-            let h = vec![*a, *b];
-            circuits.push(h);
-            true
-        },
-        (Some(c0), None) => {
-            // println!("({a},{b}) ({c0},None)");
-            circuits[c0].push(*b);
-            true
-        },
-        (None, Some(c1)) => {
-            // println!("({a},{b}) (None,{c1})");
-            circuits[c1].push(*a);
-            true
-        },
+        (None, None) => circuits.push(vec![*a, *b]),
+        (Some(c0), None) => circuits[c0].push(*b),
+        (None, Some(c1)) => circuits[c1].push(*a),
         (Some(c0), Some(c1)) => {
             if c0 == c1 {
-                // println!("({a},{b}) already in {c0}");
                 return false;
             }
-            // println!("({a},{b}) merging ({},{})",
-            //     PrintVec(circuits[c0].iter().collect_vec()),
-            //     PrintVec(circuits[c1].iter().collect_vec()),
-            // );
-            let h1 = circuits[c1].clone();
+            let h1 = circuits[c1].clone(); // borrowing
             circuits[c0].extend(h1.iter());
             circuits.swap_remove(c1);
-            true
         },
     }
+    true
 }
 
 impl Display for Vector3 {
